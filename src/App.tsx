4f17c58eefa6fe234f1e0d76101967dbb0813ff3
cooderyg <ygk9604@naver.com>
2024-11-27 21:54:34 +0900

    feat:

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { ResumeResourceApiFactory, Resume, Configuration as ResumeConfiguration } from "./react-applicant-common";
-import { JobPostingResourceApiFactory, JobPosting, Configuration as JobPostingConfiguration } from "./react-jobposting-common";
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import ApplicantGrid from './component/applicantGrid';
-import JobGrid from './component/jobGrid';
+import {
+  ResumeResourceApiFactory,
+  Resume,
+  Configuration as ResumeConfiguration,
+} from "./react-applicant-common";
+import {
+  JobPostingResourceApiFactory,
+  JobPosting,
+  Configuration as JobPostingConfiguration,
+} from "./react-jobposting-common";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import ApplicantGrid from "./component/applicantGrid";
+import JobGrid from "./component/jobGrid";
 
 function App() {
   const [resumeData, setResumeData] = useState<Resume[]>([]);
@@ -16,7 +24,9 @@ function App() {
 
   useEffect(() => {
     // applicant backend Data
-    const resumeApiConfig = new ResumeConfiguration({ basePath: "<Applicant URL>" }); // https://app.weplat.shop
+    const resumeApiConfig = new ResumeConfiguration({
+      basePath: "https://applicant.cvcontrol.shop",
+    }); // https://app.weplat.shop
     const resumeApi = ResumeResourceApiFactory(resumeApiConfig);
     resumeApi.getAllResumes(0, 10).then(async (res) => {
       const { data } = await resumeApi.getResume(1);
@@ -25,7 +35,9 @@ function App() {
     });
 
     // job backend Data
-    const jobPostingApiConfig = new JobPostingConfiguration({ basePath: "<Jobposting URL>" }); // https://job.weplat.shop
+    const jobPostingApiConfig = new JobPostingConfiguration({
+      basePath: "https://jobposting.cvcontrol.shop",
+    }); // https://job.weplat.shop
     const jobPostingApi = JobPostingResourceApiFactory(jobPostingApiConfig);
     jobPostingApi.getAllJobPostings(0, 10).then(async (res) => {
       const { data } = await jobPostingApi.getJobPosting(1);
@@ -33,27 +45,31 @@ function App() {
       setJobData(res.data);
     });
   }, []);
- 
+
   return (
     <div className="App">
       <header className="App-header">
-       
-        <Stack  direction="row">
-          </Stack>
-          <Box sx={{ width: '100%' }}>
+        <Stack direction="row"></Stack>
+        <Box sx={{ width: "100%" }}>
           <Stack spacing={2}>
             <Item>
-                <Button variant="contained" color="success">채용사이트 관리자 페이지</Button>
+              <Button variant="contained" color="success">
+                채용사이트 관리자 페이지
+              </Button>
             </Item>
             <ItemLeft>
-                <Button variant="contained">Applicant App BackEnd Data - 이력서 정보</Button>
+              <Button variant="contained">
+                Applicant App BackEnd Data - 이력서 정보
+              </Button>
             </ItemLeft>
-                <ApplicantGrid gridData={resumeData} ></ApplicantGrid>
+            <ApplicantGrid gridData={resumeData}></ApplicantGrid>
             <Item></Item>
             <ItemLeft>
-                <Button variant="contained">Jobposting App BackEnd Data - 채용공고 정보</Button>
+              <Button variant="contained">
+                Jobposting App BackEnd Data - 채용공고 정보
+              </Button>
             </ItemLeft>
-                <JobGrid gridData={jobData} ></JobGrid>
+            <JobGrid gridData={jobData}></JobGrid>
             <Item>WePLAT Cloud Infra</Item>
           </Stack>
         </Box>
@@ -63,18 +79,18 @@ function App() {
 }
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 const ItemLeft = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'left',
+  textAlign: "left",
   color: theme.palette.text.secondary,
 }));
 
